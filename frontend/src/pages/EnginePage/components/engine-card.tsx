0fbccfc248e3eb6dbb8d7b38ce6fa893e7ce12ca
Guy de Messimy <gdemessimy@gmail.com>
2024-09-30 21:42:53 +0200

    frontend: styled

--- frontend/src/pages/EnginePage/components/engine-card.tsx
+++ frontend/src/pages/EnginePage/components/engine-card.tsx
@@ -13,8 +13,8 @@ const EngineCard: FC<ReportComponentProps> = ({ engineItem }) => {
   console.log('engineItem', engineItem);
   return (
     <div className="engine__card">
-      <div className="engine__card__title">{`${engineItem.brandName} ${engineItem.modelName}`}</div>
-      <>ou similaire | moissonneuse</>
+      <div className="engine__card__title">{`${engineItem.brandName} ${engineItem.modelName}`} | ou similaire</div>
+      <>moissonneuse</>
       <img className="engine__card__photo" src={engineItem.imageUrl} alt="Machine" />
     </div>
   );
