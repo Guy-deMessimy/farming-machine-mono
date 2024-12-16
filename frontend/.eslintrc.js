16e7315834ce169651ee2a8cd1d77d3963e7f238
Guy de Messimy <gdemessimy@gmail.com>
2024-12-16 21:31:50 +0100

    frontend fix linter

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,47 +1,47 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 6,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['prettier', 'react', 'react-hooks', '@typescript-eslint'],
-    extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-      'plugin:prettier/recommended',
-    ],
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-        jest: true,
   },
-    "settings": {
-      "react": {
-        "version": "detect"
-      }
+  root: true,
+  plugins: ['prettier', 'react', 'react-hooks', '@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    ignorePatterns: ['node_modules', 'build', 'dist', 'public'],
-    rules: {
-        eqeqeq: 'error',
-        'no-console': 'warn',
-        'prettier/prettier': 'error',
-        'react/display-name': 'off',
-        'react/no-children-prop': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          {
-            argsIgnorePattern: '^_',
-            varsIgnorePattern: '^_',
-            caughtErrorsIgnorePattern: '^_',
-          },
-        ],
+  },
+  ignorePatterns: ['node_modules', 'build', 'dist', 'public'],
+  rules: {
+    eqeqeq: 'error',
+    'no-console': 'warn',
+    'prettier/prettier': 'error',
+    'react/display-name': 'off',
+    'react/no-children-prop': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
       },
-  };
+    ],
+  },
+};
