a935ab3e5845e4ff39802e6b7b8dc289270960d3
Guy de Messimy <gdemessimy@gmail.com>
2024-12-31 12:02:48 +0100

    frontend: use antdesign menu

--- frontend/src/components/MainNavigationComponent/index.tsx
+++ frontend/src/components/MainNavigationComponent/index.tsx
@@ -1,26 +1,54 @@
 import { Button } from '@mui/base/Button';
+import { useNavigate } from 'react-router-dom';
+import type { MenuProps } from 'antd';
 // Components
 import FarmingMachineLogo from '../AssetPictosComponent/iconLogo/index';
 import User from '../AssetPictosComponent/iconUser/index';
-import DropdownMenu from '../MenuComponent';
-// Utils
-import { TabDataModel } from '../utils/model-component';
+import DropdownMenu from '../MenuComponent/index';
 // UI and assets
 import './styles.scss';
 
-const myTabsData: TabDataModel[] = [
-  { value: 1, label: 'Engines', path: '/engines' },
-  { value: 2, label: 'Services', path: '/category' },
-  { value: 3, label: 'Rent', path: '/rent' },
-  { value: 4, label: 'Share', path: '/share' },
-  { value: 5, label: 'Blog', path: '/blog' },
-  { value: 6, label: 'Configuration', path: '/configuration' },
-];
-
 const MainNavigationComponent = () => {
+  const navigate = useNavigate();
+
+  const items: MenuProps['items'] = [
+    {
+      label: <div onClick={() => navigate('/engines')}>Engines</div>,
+      key: '0',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      label: <div onClick={() => navigate('/services')}>Services</div>,
+      key: '1',
+      disabled: true,
+    },
+    {
+      label: <div onClick={() => navigate('/rent')}>Rent</div>,
+      key: '2',
+      disabled: true,
+    },
+    {
+      label: <div onClick={() => navigate('/share')}>Share</div>,
+      key: '3',
+      disabled: true,
+    },
+    {
+      label: <div onClick={() => navigate('/blog')}>Blog</div>,
+      key: '4',
+      disabled: true,
+    },
+    {
+      label: <div onClick={() => navigate('/configurations')}>Configuration</div>,
+      key: '5',
+      disabled: true,
+    },
+  ];
+
   return (
     <header className="header">
-      <DropdownMenu tabsData={myTabsData} />
+      <DropdownMenu items={items} />
       <FarmingMachineLogo alt={'logo'} title={'logo_farming_machine'} width="65px" height="65px" path={'/'} />
       <h3 className="baseline"> trouvez votre farming machine</h3>
       <div className="button-group">
--- frontend/src/components/MenuComponent/index.tsx
+++ frontend/src/components/MenuComponent/index.tsx
@@ -1,59 +1,23 @@
-import React, { FC } from 'react';
-import { Dropdown, MenuButton, Menu, MenuItem, MenuListboxSlotProps, menuItemClasses } from '@mui/base';
-import { useNavigate } from 'react-router-dom';
-import { CssTransition } from '@mui/base/Transitions';
-import { PopupContext } from '@mui/base/Unstable_Popup';
-// Utils
-import { TabDataModel } from '../utils/model-component';
-// Ui and assets
-import './styles.scss';
+import React from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import { ItemType } from 'antd/es/menu/interface';
+import { Dropdown, Space } from 'antd';
 
-interface DropdownMenuProps {
-  tabsData: TabDataModel[];
+interface ReportComponentProps {
+  items: ItemType[];
 }
 
-const DropdownMenu: FC<DropdownMenuProps> = ({ tabsData }) => {
-  const navigate = useNavigate();
-
-  const createHandleMenuClick = (path: string) => () => {
-    navigate(path);
-  };
-
+const DropdownMenu: React.FC<ReportComponentProps> = ({ items }) => {
   return (
-    <Dropdown>
-      <MenuButton className="menu-button">Navigation</MenuButton>
-      <Menu className="menu" slots={{ listbox: AnimatedListbox }}>
-        {tabsData.map((tab) => (
-          <MenuItem
-            className={`menu-item ${menuItemClasses.disabled ? 'disabled' : ''}`}
-            key={tab.value}
-            onClick={createHandleMenuClick(tab.path)}
-          >
-            {tab.label}
-          </MenuItem>
-        ))}
-      </Menu>
+    <Dropdown menu={{ items }}>
+      <a onClick={(e) => e.preventDefault()}>
+        <Space>
+          Hover me
+          <DownOutlined />
+        </Space>
+      </a>
     </Dropdown>
   );
 };
-
-const AnimatedListbox = React.forwardRef(function AnimatedListbox(
-  props: MenuListboxSlotProps,
-  ref: React.ForwardedRef<HTMLUListElement>,
-) {
-  const { ownerState, ...other } = props;
-  const popupContext = React.useContext(PopupContext);
-  if (popupContext === null) {
-    throw new Error('The `AnimatedListbox` component cannot be rendered outside a `Popup` component');
-  }
-
-  const verticalPlacement = popupContext.placement.split('-')[0];
-
-  return (
-    <CssTransition className={`placement-${verticalPlacement}`} enterClassName="open" exitClassName="closed">
-      <ul {...other} ref={ref} />
-    </CssTransition>
-  );
-});
 
 export default DropdownMenu;
