7616874959264c755f884eb73e52229498056d56
deMessimy <gdemessimy@gmail.com>
2025-04-20 17:00:21 +0200

    api: add sign in

--- api/src/iam/authentication/authentication.resolver.ts
+++ api/src/iam/authentication/authentication.resolver.ts
@@ -4,6 +4,7 @@ import { PubSub } from 'graphql-subscriptions';
 import { AuthenticationService } from './authentication.service';
 import { User } from '../../modules/users/users.entity';
 import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Resolver()
 export class AuthenticationResolver {
@@ -18,4 +19,10 @@ export class AuthenticationResolver {
     // this.logger.debug(`findAllEngineTypes called in resolver with query: ${query}`);
     return this.authenticationService.signUp(input);
   }
+
+  @Mutation(() => Boolean)
+  async signIn(@Args('input') input: SignInDto): Promise<boolean> {
+    return this.authenticationService.signIn(input)
+  }
+
 }
--- api/src/iam/authentication/dto/sign-in.dto.ts
+++ api/src/iam/authentication/dto/sign-in.dto.ts
@@ -1,9 +1,15 @@
-import { IsEmail, MinLength } from 'class-validator';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, MinLength, IsString } from 'class-validator';
+
+@InputType()
 
 export class SignInDto {
+  @Field()
   @IsEmail()
   email: string;
 
+  @Field()
+  @IsString()
   @MinLength(10)
   password: string;
 }
