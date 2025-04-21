3b62393c39faf426b931a99393cb0b3a5c7c1ab7
deMessimy <gdemessimy@gmail.com>
2025-04-21 18:12:35 +0200

    backend: add signin

--- backend/src/modules/auth/auth.resolver.ts
+++ backend/src/modules/auth/auth.resolver.ts
@@ -6,6 +6,8 @@ import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { Observable } from 'rxjs';
 import { User } from '../users/users.entity';
+import { AuthPayload } from './dto/auth-payload.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Resolver()
 export class AuthResolver {
@@ -29,6 +31,24 @@ export class AuthResolver {
       return result;
     } catch (error) {
       this.logger.error('Resolver: signUp error:', error.stack);
+      throw error;
+    }
+  }
+
+  @Mutation(() => AuthPayload)
+  signIn(
+    @Context() context?: { req: Request },
+    @Args('input') input?: SignInDto,
+  ): Observable<AuthPayload> {
+    try {
+      const graphQlQuery = context.req.body.query;
+      if (typeof graphQlQuery !== 'string') {
+        throw new Error('Request body query is not a string');
+      }
+
+      return this.authService.signIn({ graphQlQuery, input });
+    } catch (error) {
+      this.logger.error('Resolver: signIn error:', error.stack);
       throw error;
     }
   }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { GraphqlApiService } from '../graphql-api/graphql-api.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { Observable } from 'rxjs';
 import { User } from '../users/users.entity';
+import { SignInDto } from './dto/sign-in.dto';
+import { AuthPayload } from './dto/auth-payload.dto';
 
 @Injectable()
 export class AuthService {
@@ -17,7 +19,19 @@ export class AuthService {
   }): Observable<User> {
     // console.log('GRAPHQL QUERY', graphQlQuery);
     // console.log('INPUT', input);
-    const response = this.graphqlApiService.execute<User>(graphQlQuery, { input });
+    const response = this.graphqlApiService.execute<User>(graphQlQuery, {
+      input,
+    });
     return response;
+  }
+
+  signIn({
+    graphQlQuery,
+    input,
+  }: {
+    graphQlQuery: string;
+    input: SignInDto;
+  }): Observable<AuthPayload> {
+    return this.graphqlApiService.execute<AuthPayload>(graphQlQuery, { input });
   }
 }
