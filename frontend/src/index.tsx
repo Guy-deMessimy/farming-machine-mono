d214d01de82b97a3e8b21c4e7614bb6100ee1bd3
deMessimy <gdemessimy@gmail.com>
2025-05-09 17:07:04 +0200

    frontend: add token in apollo context client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,33 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider, from } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { createUploadLink } from 'apollo-upload-client';
 import { onError } from '@apollo/client/link/error';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import './styles.scss';
+import { getAuthToken } from './shared/utils/auth';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-const uploadLink = createUploadLink({
+const httpLink = createUploadLink({
   uri: '/graphql' || 'http://backend:3001/graphql' || 'http://localhost:3001/graphql',
   credentials: 'include',
 });
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
-    );
+const authLink = setContext((_, { headers }) => {
+  const token = getAuthToken();
+  console.log('AAA token', token);
+  return {
+    headers: {
+      ...headers,
+      ...(token && { Authorization: `Bearer ${token}` }),
+    },
+  };
+});
 
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.warn(`[GraphQL error]: ${message}`, { locations, path });
+    });
+  }
+  if (networkError) {
+    console.error(`[Network error]:`, networkError);
+  }
 });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: from([errorLink, uploadLink]),
+  link: from([errorLink, authLink, httpLink as HttpLink]),
   connectToDevTools: true,
 });
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
@@ -36,8 +51,3 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
