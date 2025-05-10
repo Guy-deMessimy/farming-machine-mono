02adae6f3ca45f020a9a3483dde3d6efcb2eaab1
deMessimy <gdemessimy@gmail.com>
2025-05-10 10:59:12 +0200

    frontend: force window reload on logout

--- frontend/src/pages/Logout/index.tsx
+++ frontend/src/pages/Logout/index.tsx
@@ -1,7 +1,10 @@
 import { redirect } from 'react-router-dom';
+import { ApolloClient } from '@apollo/client';
 
 export const logoutAction = () => {
   localStorage.removeItem('token');
-  // localStorage.remove('expiration');
-  return redirect('/');
+  localStorage.removeItem('expiration');
+  //await client.clearStore(); // supprime tout le cache Apollo
+  window.location.href = '/auth?mode=login';
+  return null;
 };
