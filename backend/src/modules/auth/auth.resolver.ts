9903afe956c760890d3dfa239600a76b8515c950
deMessimy <gdemessimy@gmail.com>
2025-05-13 23:45:01 +0200

    backend: add cookies in res signin

--- backend/src/modules/auth/auth.resolver.ts
+++ backend/src/modules/auth/auth.resolver.ts
@@ -1,13 +1,14 @@
 // src/auth/auth.resolver.ts
 import { Resolver, Mutation, Args, Context } from '@nestjs/graphql';
 import { Logger } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/sign-up.dto';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { User } from '../users/users.entity';
 import { AuthPayload } from './dto/auth-payload.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Resolver()
 export class AuthResolver {
@@ -15,18 +16,13 @@ export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Mutation(() => User)
+  @Public()
   signUp(
     @Context() context?: { req: Request },
     @Args('input') input?: SignUpDto,
   ): Observable<User> {
     try {
       const graphQlQuery = context.req.body.query;
-      // console.log('graphQlQuery RESOLVER', graphQlQuery)
-      // console.log('input RESOLVER', input)
-      if (typeof graphQlQuery !== 'string') {
-        throw new Error('Request body query is not a string');
-      }
-
       const result = this.authService.signUp({ graphQlQuery, input });
       return result;
     } catch (error) {
@@ -36,23 +32,33 @@ export class AuthResolver {
   }
 
   @Mutation(() => AuthPayload)
-  signIn(
-    @Context() context?: { req: Request },
+  @Public()
+  async signIn(
+    @Context() ctx?: any,
     @Args('input') input?: SignInDto,
-  ): Observable<AuthPayload> {
-    const graphQlQuery = context.req.body.query;
-    return this.authService.signIn({ graphQlQuery, input });
+  ): Promise<AuthPayload> {
+    const { req, res }: { req: Request; res: Response } = ctx;
+    const graphQlQuery = req.body.query;
+    // Converts an observable to a promise by subscribing to the obervable and returning a promise that will be resolved upon the arrival of the first value from the observable. The subscription will then be close .
+    const { accessToken, user } = await firstValueFrom(
+      this.authService.signIn({ graphQlQuery, input }),
+    );
+    console.log('[🧪] Token renvoyé par AuthService:', accessToken);
+    if (!accessToken) {
+      console.error('❌ accessToken is missing');
+    }
 
-    // try {
-    //   const graphQlQuery = context.req.body.query;
-    //   if (typeof graphQlQuery !== 'string') {
-    //     throw new Error('Request body query is not a string');
-    //   }
+    // Protection for the XSS
+    // Le cookie est transmis uniquement via les en-têtes HTTP Set-Cookie, pas dans le corps JSON.
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      // secure true ne passe pas en local sans https
+      secure: process.env.NODE_ENV === 'production',
+      // sameSite: 'lax', // ou strict
+      sameSite: true,
+      // maxAge: 1000 * 60 * 60, // 1h
+    });
 
-    //   return this.authService.signIn({ graphQlQuery, input });
-    // } catch (error) {
-    //   this.logger.error('Resolver: signIn error:', error.stack);
-    //   throw error;
-    // }
+    return { user };
   }
 }
