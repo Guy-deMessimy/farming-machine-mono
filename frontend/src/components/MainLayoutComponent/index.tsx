e20bb5794869a4286b928eb3ad53bacf4ddbedf2
deMessimy <gdemessimy@gmail.com>
2025-05-24 18:04:22 +0200

    frontend: cleaning

--- frontend/src/components/MainLayoutComponent/index.tsx
+++ frontend/src/components/MainLayoutComponent/index.tsx
@@ -2,29 +2,8 @@ import { useEffect } from 'react';
 import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 import MainNavigation from '../MainNavigationComponent/index';
 import './styles.scss';
-// import { getTokenDuration } from '../../shared/utils/auth';
 
 const MainLayout = () => {
-  const token = useLoaderData();
-  const submit = useSubmit();
-
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
-
-    if (token === 'EXPIRED') {
-      submit(null, { action: 'logout', method: 'post' });
-    }
-
-    // const tokenDuration = getTokenDuration();
-    // console.log('AAA token duration', tokenDuration);
-
-    // setTimeout(() => {
-    //   submit(null, { action: 'logout', method: 'post' });
-    // }, tokenDuration);
-  }, [token, submit]);
-
   return (
     <>
       <MainNavigation />
