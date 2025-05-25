a5d54be39df952d4d674e063cb3f17548bd8e972
deMessimy <gdemessimy@gmail.com>
2025-05-25 11:25:22 +0200

    frontend: welcome page css

--- frontend/src/pages/WelcomePage/index.tsx
+++ frontend/src/pages/WelcomePage/index.tsx
@@ -1,10 +1,17 @@
+import { Fragment } from 'react/jsx-runtime';
 import './styles.scss';
 
 const WelcomePage = () => {
   return (
-    <main className="welcome-page">
-      <p>Learn more about Farming Machine</p>
-    </main>
+    <Fragment>
+      <main className="welcome-page">
+        <p>Learn more about Farming Machine</p>
+      </main>
+      <div className="other-page">
+        <h2>LOUER PERFORMANT A PRIX ATTRACTIF</h2>
+        <p>Location de machines agricoles au meilleur prix Partout en France</p>
+      </div>
+    </Fragment>
   );
 };
 
