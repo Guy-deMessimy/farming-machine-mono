6d039279c0803bb4ef7aa5272b56cbf5ea998cbd
deMessimy <gdemessimy@gmail.com>
2025-05-27 22:48:40 +0200

    frontend: appolo error catching unauthenticated error for play ping

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Apollo client
-import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink, fromPromise } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 import { onError } from '@apollo/client/link/error';
 // Redux store
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 import App from './components/App/App';
+import { REFRESH_TOKEN_MUTATION } from './graphql/authentication/mutation/refresh-token.mutation';
 import './styles.scss';
 
 const httpLink = createUploadLink({
@@ -16,14 +17,46 @@ const httpLink = createUploadLink({
   credentials: 'include',
 });
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
+let isRefreshing = false;
+let pendingRequests: (() => void)[] = [];
+
+const resolvePendingRequests = () => {
+  pendingRequests.forEach((callback) => callback());
+  pendingRequests = [];
+};
+const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
     graphQLErrors.forEach(({ message, locations, path }) => {
       console.warn(`[GraphQL error]: ${message}`, { locations, path });
     });
-  }
-  if (networkError) {
-    console.error(`[Network error]:`, networkError);
+  const hasAuthError = graphQLErrors?.some(
+    (err) => err.message === 'Invalid or expired token' || err.extensions?.code === 'UNAUTHENTICATED',
+  );
+
+  if (hasAuthError) {
+    if (!isRefreshing) {
+      isRefreshing = true;
+      return fromPromise(
+        client
+          .mutate({ mutation: REFRESH_TOKEN_MUTATION })
+          .then(() => {
+            resolvePendingRequests();
+          })
+          .catch(() => {
+            console.error('Mutation refresh token failed')
+          })
+          .finally(() => {
+            isRefreshing = false;
+          }),
+      ).flatMap(() => forward(operation));
+    } else {
+      // lorsqu'une erreur d'authentification est detectée et qu'un rafraichissement est deja en cours alors la reqeuete est mise en attente dans pendingRequest
+      return fromPromise(
+        new Promise<void>((resolve) => {
+          pendingRequests.push(() => resolve());
+        }),
+      ).flatMap(() => forward(operation));
+    }
   }
 });
 
